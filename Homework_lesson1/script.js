6e4b4883ba4ae37f81070a406026ecd49acd1b6d
TratsiakOlga <tratsiak_olga@mail.ru>
2026-04-19 00:34:13 +0300

    Добавила переменные, alert, console.log() в script

--- Homework_lesson1/script.js
+++ Homework_lesson1/script.js
@@ -1,1 +1,17 @@
-console.log('script')
+const title = "Урок по JavaScript";
+let screens = "Компьютер";
+let screenPrice = 150;
+let rollback = 10;
+let fullPrice = 160;
+let adaptive = true;
+
+alert('Урок1 выполнен!!!');
+console.log('Создаем ветку lesson01');
+
+//Проверим как выводятся переменные в консоль: (В ДЗ этого не было, от себя)
+console.log(title);
+console.log(screens);
+console.log(screenPrice);
+console.log(rollback);
+console.log(fullPrice);
+console.log(adaptive);
