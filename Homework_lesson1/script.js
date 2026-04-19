deaca2226386e3294cb9eae3a6dd5803f9e7ceb9
TratsiakOlga <tratsiak_olga@mail.ru>
2026-04-19 12:30:27 +0300

    Внесла исправление в домашнее задание к Lesson01

--- Homework_lesson1/script.js
+++ Homework_lesson1/script.js
@@ -1,17 +1,9 @@
-const title = "Урок по JavaScript";
-let screens = "Компьютер";
-let screenPrice = 150;
-let rollback = 10;
-let fullPrice = 160;
-let adaptive = true;
+let title
+let screens
+let screenPrice 
+let rollback
+let fullPrice
+let adaptive
 
 alert('Урок1 выполнен!!!');
 console.log('Создаем ветку lesson01');
-
-//Проверим как выводятся переменные в консоль: (В ДЗ этого не было, от себя)
-console.log(title);
-console.log(screens);
-console.log(screenPrice);
-console.log(rollback);
-console.log(fullPrice);
-console.log(adaptive);
