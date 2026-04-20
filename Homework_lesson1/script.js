c7a0b0ff5cf5f7bfa71c9a64ae472272aa3d9162
TratsiakOlga <tratsiak_olga@mail.ru>
2026-04-20 10:27:36 +0300

    Выполнена домашняя работа Lesson02

--- Homework_lesson1/script.js
+++ Homework_lesson1/script.js
@@ -1,9 +1,27 @@
 let title
 let screens
-let screenPrice 
+let screenPrice
 let rollback
 let fullPrice
 let adaptive
 
-alert('Урок1 выполнен!!!');
-console.log('Создаем ветку lesson01');
+title = "JavaScriptExpress";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 12500;
+rollback = 25;
+fullPrice = 35000;
+adaptive = true;
+
+console.log(title);
+console.log(fullPrice);
+console.log(adaptive);
+console.log('*******')
+console.log(`Длина строки: ${screens.length}`);
+console.log('*******')
+console.log('Cтоимость верстки экранов ', screenPrice, ' рублей');
+console.log('Cтоимость разработки сайта ' + fullPrice + ' рублей');
+console.log('*******')
+console.log(screens.toLowerCase().split(', '))
+console.log('*******')
+console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback / 100)) + '')
+console.log(`Процент отката посреднику за работу ${fullPrice * (rollback / 100)}`)
